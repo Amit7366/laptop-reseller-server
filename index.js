9a54fbcdb366245e9136dcdb38515a91578ead54
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-11-26 00:38:40 +0600

    fix commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('server is running');
+  });
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
