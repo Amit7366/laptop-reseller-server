ef79a52a80d6d0efff70765beae3938b10e2959e
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-11-26 10:54:39 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
@@ -8,6 +9,27 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7dm94fg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+  try{
+    const categoryCollecttion = client.db('reseller').collection('category');
+
+    
+
+  }
+  finally{
+
+  }
+}
+
+run().catch(console.log);
 
 app.get("/", (req, res) => {
   res.send('server is running');
