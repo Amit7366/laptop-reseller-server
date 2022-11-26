7787394dcd52742dadb55b03bcc4d32d8c08b581
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-11-26 20:55:15 +0600

    add product

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
@@ -10,32 +10,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7dm94fg.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+async function run() {
+  try {
+    const categoryCollecttion = client.db("reseller").collection("category");
+    const productCollecttion = client.db("reseller").collection("products");
+    const usersCollection = client.db("reseller").collection("users");
 
-async function run(){
-  try{
-    const categoryCollecttion = client.db('reseller').collection('category');
-    const usersCollection = client.db('reseller').collection('users');
-
-
-    app.post('/users', async (req,res) =>{
-
+    app.post("/users", async (req, res) => {
       const user = req.body;
       console.log(user);
       const result = await usersCollection.insertOne(user);
 
-
       res.send(result);
-    })
+    });
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productCollecttion.insertOne(product);
+      res.send(result);
+    });
 
 
-  }
-  finally{
+
+
+    
+  } finally {
 
   }
 }
@@ -43,7 +49,7 @@ async function run(){
 run().catch(console.log);
 
 app.get("/", (req, res) => {
-  res.send('server is running');
+  res.send("server is running");
 });
 
 app.listen(port, () => {
